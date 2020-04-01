6968c81190201f6aceff03f2d79b69775da26577
hanzotaz <asyeeqasywaree@gmail.com>
2020-04-01 16:03:06 +0800

    updated

--- .config/sowm/autostart.sh
+++ .config/sowm/autostart.sh
@@ -2,10 +2,11 @@
 
 picom &
 xfce4-power-manager &
+mpd &
 sxhkd -c /home/taz/.config/sowm/sxhkdrc &
 feh --bg-fill /home/taz/Pictures/wallpapers/59FwBTe.jpg &
 #python -m SimpleHTTPServer 8085 &
-#bash ~/.config/lemonbar/config.sh | bash ~/.config/lemonbar/lemonbar.sh &
+bash ~/.config/lemonbar/config.sh | bash ~/.config/lemonbar/lemonbar.sh &
 
 xautolock -time 10 -locker "slock" &
 dunst &
