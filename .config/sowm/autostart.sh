5d16c8f6f6bbcf9fe3fa27dc19f7a38fa9ae3c51
hanzotaz <asyeeqasywaree@gmail.com>
2020-04-02 12:46:40 +0800

    updated

--- .config/sowm/autostart.sh
+++ .config/sowm/autostart.sh
@@ -8,7 +8,7 @@ feh --bg-fill /home/taz/Pictures/wallpapers/59FwBTe.jpg &
 #python -m SimpleHTTPServer 8085 &
 bash ~/.config/lemonbar/config.sh | bash ~/.config/lemonbar/lemonbar.sh &
 
-mpd-notification &
+mpd-notification -H 127.0.0.1 -p 6600 -t 30 -m /home/taz/Music &
 xautolock -time 10 -locker "slock" &
 dunst &
 
