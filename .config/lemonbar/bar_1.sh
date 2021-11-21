1608c80e7e8554fad5313ae1250e97f1f9bfacad
hanzotaz <asyeeqasywaree@gmail.com>
2021-11-21 08:40:28 +0000

    updated

--- .config/lemonbar/bar_1.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-# import colors from pywal
-. "${HOME}/.cache/wal/colors.sh"
-
-# todo: resize bar dimension
-lemonbar -g 300x20+10+10 -B "$color0" -F "$color8" -f "InconsolataGo Nerd Font Mono -11" -dp
